0d3aafde918180c080bce77ce500f82685708e40
Caroline Humphreys <humphreysca@email.arizona.edu>
2022-01-21 09:46:00 -0700

    second query

--- database/queries/query2.py
+++ database/queries/query2.py
@@ -28,29 +28,52 @@ def main(args):
     # create DB connection
     connection = createDBConnection(config.hostName,config.userName,config.userPassword,config.dbName)
 
+    # pull the lower bound for phi_d from the command line arguements, arguement #1
+    lower_phi_d = args[1]
+
+    # pull the upper bound for phi_d from the command line arguements, arguements #2
+    upper_phi_d = args[2]
+
+    # pull the lower bound for theta_d from the command line arguements, arguement #1
+    lower_theta_d = args[5]
+
+    # pull the upper bound for theta_d from the command line arguements, arguements #2
+    upper_theta_d = args[6]
+
+    # pull the lower bound for theta_h from the command line arguements, arguement #1
+    lower_theta_h = args[3]
+
+    # pull the upper bound for theta_h from the command line arguements, arguements #2
+    upper_theta_h = args[4]
+    
     # pull the material name from the command line arguments, arguement #1
-    materialName = args[1]
+    materialName = args[10]
 
     # pull the wavelength from the command line arguments, arguments #2
-    #wavelength = args[2]
+    wavelength = args[8]
 
     # pull the AOI from the command line arguments, arguments #3
-    AOI = args[2]
+    AOI = args[7]
 
     # pull the AOC from the command line arguments, arguments #4
-    AOC = args[3]
+    AOC = args[8]
 
-    query = """SELECT m00, m01, m02, m03, m10, m20, m30, m11, m12, m13, m21, m22, m23, m31, m32, m33
+    query = """SELECT m00, m01, m02, m03, m10, m20, m30, m11, m12, m13, m21, m22, m23, m31, m32, m33, cpd.AOI, cpd.AOC
                 FROM cmmi_pixel_data as cpd
                 INNER JOIN mm_samples as mm ON cpd.sample_no = mm.sample_no AND cpd.AOI = mm.AOI AND cpd.AOC = mm.AOC
                 INNER JOIN material_samples as m ON m.sample_no = cpd.sample_no
                 INNER JOIN scattering_geometry_pixel_map as sgpm ON cpd.pixel_x = sgpm.pixel_x AND cpd.pixel_y = sgpm.pixel_y AND cpd.AOI = sgpm.AOI AND cpd.AOC = sgpm.AOC
-                WHERE material_name = %s AND sgpm.AOI = %s AND sgpm.AOC = %s;"""
-    data = (materialName,AOI,AOC)
+                WHERE sgpm.phi_d BETWEEN  %s AND %s
+                AND sgpm.theta_h BETWEEN %s and %s
+                AND sgpm.theta_d BETWEEN %s and %s
+                AND sgpm.AOI = %s 
+                AND sgpm.AOC = %s
+                AND material_name = %s;"""
+    data = (lower_phi_d,upper_phi_d,lower_theta_h,upper_theta_h,lower_theta_d,upper_theta_d,AOI,AOC,materialName)
     result = readQuery(connection, query,data)
 
     # loop over result and write to file
-    with open("out.csv", "w", newline='') as csv_file:  # Python 3 version    
+    with open("out2.csv", "w", newline='') as csv_file:  # Python 3 version    
         csv_writer = csv.writer(csv_file)
         csv_writer.writerows(result)
           
